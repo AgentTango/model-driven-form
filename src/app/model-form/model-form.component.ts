932df3f8649e0b6773652ee86d2dc706db044519
Aditya Thakur <aditya1999thakur@gmail.com>
2021-08-08 01:36:11 +0530

    added fields

--- src/app/model-form/model-form.component.ts
+++ src/app/model-form/model-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-model-form',
@@ -12,13 +12,24 @@ export class ModelFormComponent implements OnInit {
   firstName!: FormControl;
   lastName!: FormControl;
   email!: FormControl;
-  password!: FormControl;
+  dropdownSelect!:FormControl;
+  payment!:FormControl;
+  address!:FormControl;
+  city!:FormControl;
+  postalCode!:FormControl;
+  state!:FormControl;
+  phone!:FormControl;
+  country!:FormControl;
+  requirement!:FormControl;
+  fax!:FormControl;
+  selectOptions !: string[];
 
 
   ngOnInit(){
     this.createFormControls();
     this.createForm();
   }
+
   createFormControls(){
     this.firstName = new FormControl('', Validators.required);
     this.lastName = new FormControl('', Validators.required);
@@ -26,19 +37,46 @@ export class ModelFormComponent implements OnInit {
       Validators.required,
       Validators.pattern("[^ @]*@[^ @]*")
     ]);
-    this.password = new FormControl('', [
+    this.selectOptions = ['Mr.','Mrs.', 'Miss'];
+    this.dropdownSelect = new FormControl(this.selectOptions[0].toString,Validators.required);
+    this.payment = new FormControl("", Validators.required);
+    this.address = new FormControl("", Validators.required);
+    this.city = new FormControl("", Validators.required);
+    this.postalCode = new FormControl("", [
       Validators.required,
-      Validators.minLength(8)
+      Validators.maxLength(6),
+      Validators.minLength(6)
     ]);
+    this.state = new FormControl("", Validators.required);
+    this.phone = new FormControl("", [Validators.required, Validators.maxLength(14),
+      Validators.minLength(10)]);
+    this.country = new FormControl("", Validators.required);
+    this.requirement = new FormControl("", Validators.required);
+    this.fax = new FormControl("", Validators.required);
   }
+
   createForm(){
     this.myform = new FormGroup({
+      
+      dropdownSelect: this.dropdownSelect,
       name: new FormGroup({
         firstName: this.firstName,
         lastName: this.lastName,
       }),
       email: this.email,
-      password: this.password,
+      fax: this.fax,
+      city: this.city,
+      state: this.state,
+      country: this.country,
+      address: this.address,
+      phone: this.phone,
+      payment: this.payment,
+      requirement: this.requirement,
+      postalCode: this.postalCode,
     });
+  }
+
+  onRegister(){
+    console.log(this.myform.value)
   }
 }
